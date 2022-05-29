4639a6866f1f4fc7534acc3969b971d12732b192
henriquegouveia1 <henriquegs002@gmail.com>
2022-05-28 22:46:40 -0300

    primeiras mod/criação e varredura de matriz

---
+++ Colecoes/Program.cs
@@ -0,0 +1,15 @@
+﻿int[,] matriz = new int[4, 2]
+    {
+        {1,2},
+        {1,2},
+        {1,2},
+        {1,2}
+    };
+
+    for (int i = 0; i < matriz.GetLength(0); i++)
+    {
+        for (int j = 0; j < matriz.GetLength(1); j++)
+        {
+            System.Console.WriteLine($"Elemento {matriz[i,j]}");
+        }
+    }
